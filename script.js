c7d4ef538aa20dc18c61c81052d18d882b73257d
rafaelcaires99 <166944789+rafaelcaires99@users.noreply.github.com>
2025-07-06 20:37:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.innerText = "Seja bem-vindo ao meu site! Aqui colocarei meus objetivos, conquistas e curiosidades.";
+}
